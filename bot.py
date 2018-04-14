c477df20b0a80d47c7123f370b6a77bd828bbb46
Evan Walters <evanwalters@protonmail.com>
2018-04-14 14:50:03 +0100

    fixed eval

--- bot.py
+++ bot.py
@@ -32,52 +32,34 @@ async def announce(ctx, channel : discord.Channel, role : discord.Role, *, conte
     await bot.send_message(channel, msg)
 
 #eval
-@commands.command(pass_context=True, hidden=True, name='eval')
-async def _eval(self, ctx, *, body: str):
-        """Evaluates a code"""
+@commands.command(pass_context=True, hidden=True)
+    @checks.is_owner()
+    async def eval(self, ctx, *, code : str):
+        """Evaluates code."""
+        code = code.strip('` ')
+        python = '```py\n{}\n```'
+        result = None
 
         env = {
             'bot': self.bot,
             'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
             'message': ctx.message,
-            '_': self._last_result
+            'server': ctx.message.server,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author
         }
 
         env.update(globals())
 
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
-
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
         try:
-            exec(to_compile, env)
+            result = eval(code, env)
+            if inspect.isawaitable(result):
+                result = await result
         except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+            return
 
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                self._last_result = ret
-                await ctx.send(f'```py\n{value}{ret}\n```')
+        await self.bot.say(python.format(result))
     
 
-bot.run("tomato") # TOKEN
+bot.run("cucumber") # TOKEN
